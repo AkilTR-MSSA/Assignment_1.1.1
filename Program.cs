b50d76773ed21eb965c318defdb85392d1dbd543
Akil Turner-Richards <ki1orich@gmail.com>
2025-04-07 14:03:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string userName = "John Doe";
+int userAge = 0;
+string userAddress = "Seattle, WA";
+
+Console.WriteLine("Enter your name:");
+userName = Console.ReadLine();
+Console.WriteLine("Enter your age:");
+userAge = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your address:");
+userAddress = Console.ReadLine();
+
+Console.WriteLine("Name: " + userName);
+Console.WriteLine("Age: " +  userAge);
+Console.WriteLine("Address: " +  userAddress);
